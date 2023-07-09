8130d068ca3ec0b542fcd477d97bbde5ae4d4e6b
Edgar Nwajei <edgarlinked@gmail.com>
2023-07-09 17:04:02 +0100

    updated

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submitbtn', (e) => {
+  let messages = []
+if (email.value === '' || email.value == null){
+  messages.push('email is required')
+}
+
+if( messages.length > 0){
+e.preventDefault()
+errorElement.innerText = messages.join(',')
+}
+
+})
